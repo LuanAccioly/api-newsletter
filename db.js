1d6bc1224a264d8e02e0875517f237fc85e2598f
Luan Accioly <acciolyluan@gmail.com>
2023-10-31 01:45:12 -0300

    feat: add db connection to async function

--- db.js
+++ db.js
@@ -2,17 +2,20 @@ import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const db_pass = process.env.DB_PASS;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
 mongoose.connect(
-  `mongodb+srv://accioly:${db_pass}@ufrpe-newsletter.c39wedp.mongodb.net/?retryWrites=true&w=majority`,
+  `mongodb+srv://${user}:${pass}@ufrpe-newsletter.c39wedp.mongodb.net/?retryWrites=true&w=majority`,
   { dbName: "newsletter" }
 );
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+async function db() {
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
 
-db.once("open", async function () {
-  console.log("Database connected successfully");
-});
+  db.once("open", async function () {
+    console.log("Database connected successfully");
+  });
+}
 
 export default db;
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import emailRoutes from "./src/routes/emailRoutes.js";
 
 import express from "express";
 import cors from "cors";
+import db from "./db.js";
 const app = express();
 
+db();
 app.use(express.json());
 app.use(cors());
 app.use("/", emailRoutes);
