f399c3b0bd2747fc048364d508bb7d178ec82ea6
Luan Accioly <acciolyluan@gmail.com>
2024-01-07 16:01:40 -0300

    refactor: remove unused email route and middleware

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -1,23 +1,13 @@
-import {
-  getEmails,
-  setEmail,
-  deleteEmail,
-  countEmails,
-  sendNews,
-} from "../controllers/email.js";
+import { setEmail, deleteEmail, countEmails } from "../controllers/email.js";
 import express from "express";
 import * as dotenv from "dotenv";
-import { allowOnlyIP } from "../middleware/allowedIP.js";
 
-const allowedIP = process.env.ALLOWED_IP;
 const router = express.Router();
 
 dotenv.config();
 
-router.get("/email/all", getEmails);
 router.get("/email/count", countEmails);
 router.post("/email/new", setEmail);
-router.post("/email/send", allowOnlyIP(allowedIP), sendNews);
 router.delete("/email/delete", deleteEmail);
 
 export default router;
